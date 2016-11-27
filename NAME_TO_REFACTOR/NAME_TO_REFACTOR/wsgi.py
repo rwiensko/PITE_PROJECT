09bd0d2e855b02aee916ea4b8cf9d4d9f8430cd7
rwiensko <rafal.wiensko@gmail.com>
2016-11-27 23:44:40 +0100

    initial commit

---
+++ NAME_TO_REFACTOR/NAME_TO_REFACTOR/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NAME_TO_REFACTOR project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NAME_TO_REFACTOR.settings")
+
+application = get_wsgi_application()
