6d346e7df06b5aa3cb47d3775f08f0f60b555cf2
rwiensko <rafal.wiensko@gmail.com>
2016-12-19 04:23:57 +0100

    fixing problem with database

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Friends(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_of_friends = ArrayField(models.IntegerField())
+
