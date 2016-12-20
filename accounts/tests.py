270cca5724fd6d8376f659313e22f13f7b095d3a
rwiensko <rafal.wiensko@gmail.com>
2016-12-20 22:10:03 +0100

    more changes in travis

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from datetime import datetime
+from django.contrib.auth.models import User
+
+from accounts.models import Profile
+
+
+class TestAccounts(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('test','test@test.pl','test')
+        self.profile = Profile(self.user,[],'test',datetime.today())
+
+    def test_set_up(self):
+        self.assertEqual(self.user.username,'test')
