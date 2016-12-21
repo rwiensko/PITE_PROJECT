1e514a649f4ab3391bca91a744581dc1508126d3
Wojtek Zygmuntowicz <wzygmuntowicz.zygmuntowicz@gmail.com>
2016-12-21 11:08:08 +0100

    Include view tests to chat app

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from chat.models import Room, Message
 
-# Create your tests here.
+class ViewTests(TestCase):
+    def test_about(self):
+        r = self.client.get('/chat/')
+        self.assertTemplateUsed(r, 'chat/about.html')
+
+    def test_new_room(self):
+        r = self.client.get('/chat/new/')
+        _, room_label = r['Location'].strip('/').split('/')
+        assert Room.objects.filter(label=room_label).exists()
+
+    def test_chat_room(self):
+        room = Room.objects.create(label='room1')
+        r = self.client.get('/chat/room1/')
+        self.assertTemplateUsed(r, 'chat/room.html')
+        assert r.context['room'] == room
