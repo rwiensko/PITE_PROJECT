598b3a53f2e5559986e08dca34b7370bc5cbdadc
Wojtek Zygmuntowicz <wzygmuntowicz.zygmuntowicz@gmail.com>
2017-01-03 12:54:34 +0100

    Remove comments from chat/consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,10 +9,6 @@ log = logging.getLogger(__name__)
 
 @channel_session
 def ws_connect(message):
-    # Extract the room from the message. This expects message.path to be of the
-    # form /chat/{label}/, and finds a Room if the message path is applicable,
-    # and if the Room exists. Otherwise, bails (meaning this is a some othersort
-    # of websocket). So, this is effectively a version of _get_object_or_404.
     try:
         _, prefix, label = message['path'].decode('ascii').strip('/').split('/')
         if prefix != 'chat':
@@ -29,15 +25,12 @@ def ws_connect(message):
     log.debug('chat connect room=%s client=%s:%s',
         room.label, message['client'][0], message['client'][1])
 
-    # Need to be explicit about the channel layer so that testability works
-    # This may be a FIXME?
     Group('chat-'+label, channel_layer=message.channel_layer).add(message.reply_channel)
 
     message.channel_session['room'] = room.label
 
 @channel_session
 def ws_receive(message):
-    # Look up the room from the channel session, bailing if it doesn't exist
     try:
         label = message.channel_session['room']
         room = Room.objects.get(label=label)
@@ -48,8 +41,6 @@ def ws_receive(message):
         log.debug('recieved message, buy room does not exist label=%s', label)
         return
 
-    # Parse out a chat message from the content text, bailing if it doesn't
-    # conform to the expected message format.
     try:
         data = json.loads(message['text'])
     except ValueError:
@@ -65,7 +56,6 @@ def ws_receive(message):
             room.label, data['handle'], data['message'])
         m = room.messages.create(**data)
 
-        # See above for the note about Group
         Group('chat-'+label, channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())})
 
 @channel_session
